5e1e8134c06abb4a2f241b146e483ce8f79779ba
SHMANTA <101752235+SHMANTA@users.noreply.github.com>
2022-03-31 20:12:42 +0300

    Update mainn.py

--- ihate_git/mainn.py
+++ ihate_git/mainn.py
@@ -1,6 +1,6 @@
 from pronto_parser import result_pronto, menu
-'''from allo_parser import result_allo
-from vivat_parser import result_vivat'''
+from allo_parser import result_allo
+'''from vivat_parser import result_vivat'''
 
 import xlsxwriter as xlsx
 import pandas as pd
@@ -17,7 +17,6 @@ def correcr_pronto(weight):
 def pronto_data(result, worksheet):
     prices = []
     weight = []
-    print(result)
     for i in result.keys():
         for j in range(len(result[i]) - 1):
             if result[i][j].split(':')[1][0] != '"' and result[i][j + 1].split(':')[1][0] != '"':
@@ -30,9 +29,23 @@ def pronto_data(result, worksheet):
     for i in range(2):
         prices.remove('')
         weight.remove('')
-    print(weight, len(weight), prices, len(prices), sep='\n\n')
     worksheet.write_column('B3', prices)
     worksheet.write_column('C3', weight)
+
+def allo_data(result, worksheet):
+    menu = ['Маргарита', 'Пепперони', 'Четыре сыра', 'Ветчина и грибы', 'Мясная', 'Мехико', 'Морская де Люкс']
+
+    prices = []
+    weight = []
+
+    for i in menu:
+        weight.extend(['', '', result[i][0], result[i][2], result[i][6], '', '', '', result[i][4], result[i][8]])
+        prices.extend(['', '', result[i][1], result[i][3], result[i][7], '', '', '', result[i][5], result[i][9]])
+    for i in range(2):
+        prices.remove('')
+        weight.remove('')
+    worksheet.write_column('F3', prices)
+    worksheet.write_column('G3', weight)
 
 menu = ["Маргарита","Пепперони","4 сыра","С ветчиной и грибами","Мясная","Мексиканская","Сальмоне"]
 pizza = ['Пиццы', 'Пронто', '', 'Виват', '',  'Аллоха']
@@ -44,6 +57,8 @@ temp_arr = []
 workbook = xlsx.Workbook('pizza_table.xlsx')
 worksheet = workbook.add_worksheet()
 bold_cell = workbook.add_format({'bold': True})
+
+
 for i in range(len(menu)):
     temp_arr.extend([menu[i] + ' маленькая', menu[i] + ' средняя', menu[i] + ' большая', '', '',
                      menu[i] + ' мальнькая тонк', menu[i] + ' средняя тонк', menu[i] + ' большая тонк', '', ''])
@@ -52,7 +67,9 @@ for i in range(len(menu)):
 worksheet.write_column('A3', temp_arr)
 worksheet.write_row('A2', tags)
 worksheet.write_row('A1', pizza)
+
 pronto_data(result_pronto, worksheet)
+allo_data(result_allo, worksheet)
 
 #button(worksheet)
 worksheet.set_row(0, 15, bold_cell)
